fe78e725538ac189d7170d0c6e78d2b34720602d
Nicolosh111 <nicoloshnewar@gmail.com>
2025-08-19 18:03:15 +0545

    Add full project files

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Homepage from './Pages/Homepage'
+import { BrowserRouter ,Routes , Route } from 'react-router-dom'
+import About from './Pages/Abouts'
+import Abouts from './Pages/Abouts'
+import Projects from './Pages/Projectsp'
+import Projectsp from './Pages/Projectsp'
+import Contractus from './Pages/Contractus'
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/abouts" element={<Abouts />} />
+        <Route path="/Projectsp" element={<Projectsp />} />
+        <Route path='/Contractus' element={<Contractus />}/>
+
+
+        
+        
+
+       
+        
+        
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import HeroSection from '../Components/Herosection/Herosection'
+
+import Skills from '../Components/Skills/Skills'
+import Footer from '../Components/Footer/Footer'
+import About from '../Components/Abouts/About'
+import Projects from '../Components/Projects/Projects'
+import ContactPage from '../Components/Contactus/ContactPage'
+
+
+const Homepage = () => {
+  return (
+    <>
+    <Navbar />
+    <HeroSection />
+    <About />
+    <ContactPage />
+    <Projects />
+
+   
+   
+    <Skills />
+    <Footer />
+   
+   
+    
+    
+   
+   
+    
+   
+ 
+  
+      
+    
+    </>
+  )
+}
+
+export default Homepage
